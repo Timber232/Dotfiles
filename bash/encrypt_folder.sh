bdb0b9ebfc3727161976ea20b6e5f81e8d397d3d
Timothy Haryadi <timong09@gmail.com>
2017-03-14 23:24:46 -0400

    Added GPG encrypt folder script

---
+++ bash/encrypt_folder.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+filepath="$1"
+recipient="$2"
+
+for d in ./$filepath/*/; do
+    # Delete encrypted files if exists
+    if test -e "./$filepath/$(basename $d).tar.gz.gpg"; then
+        rm "./$filepath/$(basename $d).tar.gz.gpg"
+    fi
+
+    # Tar and encrypt (remove the original tar afterwards)
+    tar -czvf "./$filepath/$(basename $d).tar.gz" $d && gpg -r $recipient --encrypt "./$filepath/$(basename $d).tar.gz" && rm "./$filepath/$(basename $d).tar.gz"	
+done
